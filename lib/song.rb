55252adcb218a5550bab09ea16b6cf9c26974526
patrick-rush <patrick.a.rush@gmail.com>
2020-06-28 17:01:00 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+def Song
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        all.clear
+    end
+
+    def self.create
+        self.new.save
+    end
+
+    def save
+        all << self
+    end
+
+end
